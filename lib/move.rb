857a397396c67351aac1e31de6befb9a3bcb28e8
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-05 22:13:55 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,5 +9,7 @@ end
 # code your input_to_index and move method here!
 input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-def input_to_index(input)
-  puts 
+def input_to_index(input.to_i)
+  puts "1: #{input[0]}, 2: #{input[1]}, 3: #{input[2]}, 4: #{input[3]}, 5:#{input[4]}, 6: #{input[5]}, 7: #{input[6]},
+  8: #{input[7]}, 9: #{input[8]}"
+end
